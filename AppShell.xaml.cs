e3c26f9e11794d84f2fa5306dd01cf0741f13bd5
Jahir Medina <Inferno2@gmail.com>
2025-11-10 20:08:13 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GreenCoinMovil
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GreenCoinMovil")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GreenCoinMovil.Pages")]
